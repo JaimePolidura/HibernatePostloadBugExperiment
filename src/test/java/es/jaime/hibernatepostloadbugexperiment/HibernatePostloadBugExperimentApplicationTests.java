d1b1a4e5d96ba71a5d8118e7f8b00229c0eefd2b
POLIDURA <jaime.polidura@kapsch.net>
2025-01-31 11:45:16 +0100

    Added entity-manager branch

--- src/test/java/es/jaime/hibernatepostloadbugexperiment/HibernatePostloadBugExperimentApplicationTests.java
+++ src/test/java/es/jaime/hibernatepostloadbugexperiment/HibernatePostloadBugExperimentApplicationTests.java
@@ -4,11 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import es.jaime.hibernatepostloadbugexperiment.entity.Child;
 import es.jaime.hibernatepostloadbugexperiment.entity.Parent;
-import es.jaime.hibernatepostloadbugexperiment.repository.ChildRepository;
-import es.jaime.hibernatepostloadbugexperiment.repository.ParentRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import java.util.List;
-import java.util.UUID;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,26 +15,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HibernatePostloadBugExperimentApplicationTests {
   @Autowired
-  private ParentRepository parentRepository;
-  @Autowired
-  private ChildRepository childRepository;
+  private EntityManager entityManager;
 
   @BeforeEach
+  @Transactional
   public void setup() {
-    parentRepository.deleteAll();
-    childRepository.deleteAll();
+    entityManager.createQuery("DELETE FROM Parent").executeUpdate();
+    entityManager.createQuery("DELETE FROM Child").executeUpdate();
   }
 
   @Test
+  @Transactional
   void contextLoads() {
     Parent parent = new Parent();
-    parentRepository.save(parent);
-    childRepository.save(new Child(1, parent));
-    childRepository.save(new Child(2, parent));
-    childRepository.save(new Child(3, parent));
+    entityManager.persist(parent);
 
-    List<Child> children = childRepository.findAll();
+    entityManager.persist(new Child(1, parent));
+    entityManager.persist(new Child(2, parent));
+    entityManager.persist(new Child(3, parent));
 
+    entityManager.flush();
+    entityManager.clear();
+
+    List<Child> children = entityManager.createQuery("SELECT c FROM Child c").getResultList();
     assertEquals(3, children.size());
   }
 }
